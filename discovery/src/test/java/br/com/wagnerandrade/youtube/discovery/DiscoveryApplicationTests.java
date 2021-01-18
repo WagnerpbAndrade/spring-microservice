cf0898bd47864c81ecd697d7128ab89dffca66d2
Wagner Andrade <wagnerpbandrade@gmail.com>
2021-01-18 05:05:25 -0300

    Criado o módulo 'discovery' de service discovery para registro dos microsserviços.

---
+++ discovery/src/test/java/br/com/wagnerandrade/youtube/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wagnerandrade.youtube.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
